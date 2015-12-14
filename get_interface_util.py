2e4b64d0ed964482e40bae75f24b96a3810ac70d
Scott Gardner <sgardne@gmail.com>
2015-12-14 10:02:51 -0600

    Started over. Planning on using netmiko instead of jnpr-junos

--- get_interface_util.py
+++ get_interface_util.py
@@ -1,65 +1,39 @@
 #!/usr/bin/env python
 
-from jnpr.junos import Device
-from jnpr.junos.exception import (
-        ConnectError, ProbeError, ConnectAuthError, ConnectRefusedError, 
-        ConnectTimeoutError)
+"""
+Specifically written for Python 2.6 because that's what I have.
+
+Get the % of utilized interfaces on the stack. Do not include feeds or other
+non-access ports.
+"""
+
+# RPC calls can be returned in XML format in Junos by passing "| display xml"
+# to the command. Can I use the json module to parse the XML to a dict?
 
 import ConfigParser
-from getpass import getpass
-from lxml import etree
+class AccessInterfaceUtilization:
+    """
+    Object containing utilization stats of a switch
+    """
+    def __init__(self, device):
+        self.creds = self.__createDefaults()
+        if self.creds:
+            self.u = self.creds[0]
+            self.p = self.creds[1]
 
-config = ConfigParser.ConfigParser()
-config.read('default.ini')
+    def __createDefaults(self):
+        try:
+            self.config = ConfigParser.ConfigParser()
+            self.config.readfp(open('default.ini'))
+            return [self.config.get('DEFAULT', 'username'),
+                self.config.get('DEFAULT', 'password')]
+        except IOError as ioe:
+            print("Could not open default.ini: {0}".format(ioe))
+        except ConfigParser.Error as e:
+            print("ConfigParser died: {0}".format(e))
 
-u = ""
-p = ""
+def main():
+    util = AccessInterfaceUtilization("tacos")
 
-count = 0
-while count < 3 and not p or p is "":
-    if count > 0:
-        print("I didn't get that...")
-
-    u = raw_input("Username: ")
-    p = getpass()
-    count += 1
-
-if u is "" or p is "":
-    print("Username or password not supplied, using defaults...")
-    u = config.get('DEFAULT', 'username')
-    p = config.get('DEFAULT', 'password')
-
-Device.auto_probe = 10
-
-switch = Device(host="10.56.133.100", user=u, password=p)
-
-try:
-    devOpenHandle = switch.open()
-
-
-except ConnectAuthError as authe:
-    print "Authentication failed: {0}".format(authe)
-    # Try to get their credentials again
-except ProbeError as pe:
-    print "There was an error probing for NETCONF: {0}".format(pe)
-    # log this
-except ConnectRefusedError as cr:
-    print "Connection was refused: {0}".format(cr)
-except ConnectTimeoutError as ct:
-    print "Connection timed out: {0}".format(ct)
-
-print(etree.tostring(switch.rpc.get_interface_information(terse=True),
-    pretty_print=True))
-
-# Get number of access interfaces
-# I think this can be done simply by grabbing all the ge-* interfaces and
-# excepting anything that's tagged. Need to make sure that interfaces that
-# are not configured are counted. Might have to do this simply with model
-# numbers since they don't seem to show up in either ethport or phyport
-# tables.
-
-# Determine % utilization - divide active interfaces by available
-# interfaces.
-# Spit out the number * 100
-
-switch.close()
+if __name__ == "__main__":
+    main()
