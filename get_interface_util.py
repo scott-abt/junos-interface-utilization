0963594f0bd0abba2946e8e71bfe3cdced08f50a
Scott Gardner <sgardne@uark.edu>
2015-12-17 16:00:44 -0600

    messing with options to try to make something sensible.

--- get_interface_util.py
+++ get_interface_util.py
@@ -7,7 +7,7 @@ Specifically written for Python 2.6 because that's what I have. Get the % of uti
 non-access ports.
 """
 
-import getpass, sys, xmltodict
+import getpass, sys, xmltodict, re
 from netmiko import ConnectHandler
 from argparse import ArgumentParser as AP
 from argparse import FileType
@@ -36,10 +36,6 @@ class AccessInterfaceUtilization:
             print("Error opening " + self.ini_file + " {0}".format(ioe))
 
         if len(self.switch_dict.keys()) > 0:
-            # Need to create better way to get config for portability. Multiple
-            # devices with multiple default creds.
-            self.switch_dict['username'] = self.cfg.get('sgardne', 'username')
-            self.switch_dict['password'] = self.cfg.get('sgardne', 'password')
             self.op_rpc = str(
                     'show ethernet-switching interfaces detail | display xml')
 
@@ -48,12 +44,9 @@ class AccessInterfaceUtilization:
             self.xml_output = self.conn.send_command(self.op_rpc)
 
             if self.switch_dict['username'] == "root":
-                print("User is root")
                 self.clean_xml = str(self.xml_output).partition("\n")[2]
             else:
-                print("User is not root: " + self.switch_dict['username'])
                 self.clean_xml = str(self.xml_output).strip().partition("\n")[2]
-
             
             self.dict_of_xml = xmltodict.parse(self.clean_xml)
             for interface in self.dict_of_xml['rpc-reply']['switching-interface-information']['interface']:
@@ -69,24 +62,29 @@ class AccessInterfaceUtilization:
 def main():
     arg_parser = AP()
     
-    arg_parser.add_argument('-u', dest='username',
+    arg_parser.add_argument('username',
             help='Switch user with operator priveleges')
-    arg_parser.add_argument('-f', dest='cfg_file', 
+    arg_parser.add_argument('-p', dest='password',
+            action='store_true',
+            default=False,
+            help='Configure the switch password')
+    arg_parser.add_argument('cfg_file', 
             default="SWITCH_LIST",
             help="Imports list of dicts from a local file. default is "
             "SWITCH_LIST.py")
     result = arg_parser.parse_args()
+
+    if result.password:
+        switch_pass = getpass.getpass()
+    else:
+        switch_pass = ""
     
     SWITCH_LIST = __import__(result.cfg_file)
-    print(SWITCH_LIST.the_list[0]['ip'])
-
-
-    ## Build the switch dict and create the object.
-
-#    for each_switch in the_list:
-#        print("Trying " + each_switch['ip'])
-#        util = AccessInterfaceUtilization("mycreds.ini", each_switch)
-#        print(util.up_access_interfaces)
+    for switch_dict in SWITCH_LIST.the_list:
+        switch_dict['username'] = result.username
+        switch_dict['password'] = switch_pass
+        arp_table = AccessInterfaceUtilization("mycreds.ini", switch_dict)
+        print(arp_table.up_access_interfaces)
 
 if __name__ == "__main__":
     main()
